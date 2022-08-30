00fe86f6c3c200f3adf07534c8257c42daa77c8a
김지호 <crsn1111@gmail.com>
2022-08-30 22:43:32 +0900

    [chore] install packing

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./layouts/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode:"class"
+};
