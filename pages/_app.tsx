e51f9b819b590a47ec818797802bfd5aef21f5d3
김지호 <crsn1111@gmail.com>
2022-08-30 22:46:43 +0900

    [feat] getStaticProps of layout render change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import HeaderMain from "../components/layouts/Main/HeaderMain";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import React from "react";
+import SideSection from "../components/layouts/Main/SideSection";
+import LayoutMain from "../components/layouts/Main/LayoutMain";
+import { RecoilRoot } from "recoil";
+import DarkContainer from "../components/layouts/Main/DarkContainer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+  switch (pageProps.layout) {
+    case "intro": {
+      return <Component {...pageProps} />;
+    }
+    default: {
+      return (
+        <RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <ReactQueryDevtools initialIsOpen={true} />
+              <DarkContainer>
+                <HeaderMain />
+                <LayoutMain>
+                  <SideSection />
+                  <Component {...pageProps} />
+                </LayoutMain>
+              </DarkContainer>
+            </Hydrate>
+          </QueryClientProvider>
+        </RecoilRoot>
+      );
+    }
+  }
 }
 
-export default MyApp
+export default MyApp;
