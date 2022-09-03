dbe6634fe7c1daeeb0d83fd18f545b806b6e1d78
김지호 <crsn1111@gmail.com>
2022-09-03 22:33:11 +0900

    [feat] 문서 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head lang="kr">
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head lang="kr"></Head>
         <body>
           <Main />
           <NextScript />
