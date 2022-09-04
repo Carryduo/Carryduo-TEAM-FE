f0899fa4db00732bc7020c48a53dcbb5059fd921
김지호 <crsn1111@gmail.com>
2022-09-04 18:16:13 +0900

    [style] 그리드 틀 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import Grid from "../components/common/Grid";
 import PageContainer from "../components/common/PageContainer";
 
 const MainPage = () => {
-  return <PageContainer>첫 페이지는 뭐가 좋을까</PageContainer>;
+  return (
+    <PageContainer>
+      <Grid width="w-[600px]" height="100%">
+        <span>챔피언 리스트</span>
+      </Grid>
+      <Grid width="w-full" height="100%">
+        <span>듀오 승률 리스트</span>
+      </Grid>
+    </PageContainer>
+  );
 };
 
 export default MainPage;
