e8e0d008f97b7344137a349f10f307ac9cb06263
김지호 <crsn1111@gmail.com>
2022-09-04 18:15:21 +0900

    [feat] font chagne

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head lang="kr"></Head>
+        <Head lang="kr">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Qwitcher+Grypen&display=swap" rel="stylesheet"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
