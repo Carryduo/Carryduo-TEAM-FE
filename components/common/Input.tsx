b8c48cc20790e68bcc9386af7132f7f438fe4eb0
김지호 <crsn1111@gmail.com>
2022-09-04 18:13:31 +0900

    [feat] add opacity option

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -3,12 +3,27 @@ import { UseFormRegisterReturn } from "react-hook-form";
 interface InputProps {
   register: UseFormRegisterReturn;
   type: "text" | "password" | "email" | "file";
-  autoFocus: boolean;
-  placeHolder : string;
+  autoFocus?: boolean;
+  placeHolder: string;
+  opacity?: number;
 }
 
-const Input = ({ register, type, autoFocus , placeHolder }: InputProps) => {
-  return <input placeholder={placeHolder} autoFocus={autoFocus} {...register} type={type} />;
+const Input = ({
+  register,
+  type,
+  autoFocus,
+  placeHolder,
+  opacity,
+}: InputProps) => {
+  return (
+    <input
+      className={`h-8 w-[300px] rounded-2xl bg-white bg-opacity-[${opacity}%] px-4 outline-none text-white`}
+      placeholder={placeHolder}
+      autoFocus={autoFocus}
+      {...register}
+      type={type}
+    />
+  );
 };
 
 export default Input;
