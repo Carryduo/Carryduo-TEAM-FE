e255218ff213c9546954c8b3dd1a06dc665c5109
김지호 <crsn1111@gmail.com>
2022-09-04 18:14:45 +0900

    [feat] 문서 수정

--- pages/profile/[myNickName].tsx
+++ pages/profile/[myNickName].tsx
@@ -1,31 +1,12 @@
 import Image from "next/image";
 import { useRecoilValue } from "recoil";
 import PageContainer from "../../components/common/PageContainer";
-import { MyProfile } from "../../config/Recoil/MyProfile";
 
 const Profile = () => {
-  const { nickName, image, mostPick } = useRecoilValue(MyProfile);
-
   return (
     <PageContainer>
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <Image alt="" width={100} height={100} src={image} />
-        <span>{nickName}</span>
         <span>Challenger 1023p</span>
-        <div className="flex w-1/4 justify-around">
-          {mostPick.map((data) => {
-            return (
-              <Image
-                className="rounded-full"
-                key={data}
-                alt=""
-                width={50}
-                height={50}
-                src={data}
-              />
-            );
-          })}
-        </div>
         <div>모스트 픽 캐릭터 변경</div>
       </div>
     </PageContainer>
