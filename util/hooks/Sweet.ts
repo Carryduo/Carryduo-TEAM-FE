d6100c34633472ded9a285e18bf9327589b91254
김지호 <crsn1111@gmail.com>
2022-09-05 13:01:40 +0900

    [feat] useSweet

---
+++ util/hooks/Sweet.ts
@@ -0,0 +1,19 @@
+import Swal from "sweetalert2";
+
+// SweetAlert CustomHook
+export const useSweet = (timer: any, icon: any, title: any) => {
+  Swal.mixin({
+    toast: true,
+    position: "top",
+    showConfirmButton: false,
+    timer,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  }).fire({
+    icon,
+    title,
+  });
+};
