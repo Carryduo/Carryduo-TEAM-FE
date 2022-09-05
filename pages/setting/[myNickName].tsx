72a1cb5f38726dd6ae08a227df98654555803d4f
김지호 <crsn1111@gmail.com>
2022-09-05 19:02:48 +0900

    [feat] 기능 추가

--- pages/setting/[myNickName].tsx
+++ pages/setting/[myNickName].tsx
@@ -1,13 +1,19 @@
+import { useSetRecoilState } from "recoil";
 import Grid from "../../components/common/Grid";
 import PageContainer from "../../components/common/PageContainer";
 import Seo from "../../components/common/Seo";
+import { getShadowColor } from "../../core/config/shadowColor";
 
 const Profile = () => {
+  const setColor = useSetRecoilState(getShadowColor);
   return (
     <PageContainer>
       <Seo title="설정" />
       <Grid width="w-full" height="h-full">
         <span>설정</span>
+        <span onClick={() => setColor(1)}>1</span>
+        <span onClick={() => setColor(2)}>2</span>
+        <span onClick={() => setColor(3)}>3</span>
       </Grid>
     </PageContainer>
   );
