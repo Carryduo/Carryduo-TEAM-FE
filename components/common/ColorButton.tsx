ddcb614584e9394cd1d3a11befc596865420fbe0
김지호 <crsn1111@gmail.com>
2022-09-06 23:37:03 +0900

    [style] 테두리 색상 컴포넌트

---
+++ components/common/ColorButton.tsx
@@ -0,0 +1,19 @@
+import { useSetRecoilState } from "recoil";
+import { getShadowColor } from "../../core/config/shadowColor";
+
+interface ColorButtonProps {
+  colorNumber: number;
+  backgroundRing: string;
+}
+
+const ColorButton = ({ colorNumber, backgroundRing }: ColorButtonProps) => {
+  const setColor = useSetRecoilState(getShadowColor);
+  return (
+    <button
+      onClick={() => setColor(colorNumber)}
+      className={`h-5 w-5 rounded-full ${backgroundRing} ring-offset-2 transition focus:ring-2`}
+    />
+  );
+};
+
+export default ColorButton;
