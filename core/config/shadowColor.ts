040fa19c603b65841ba8c9a639c3a44d4430a109
김지호 <crsn1111@gmail.com>
2022-09-06 23:38:27 +0900

    [docs] 문서 수정

--- core/config/shadowColor.ts
+++ core/config/shadowColor.ts
@@ -3,7 +3,6 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-/* 전체 차트 그래프 정보 저장 */
 export const getShadowColor = atom<number>({
   key: "shadowColor",
   default: 1,
