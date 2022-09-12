b48f7d4364fb8e1a56c364e3d6f3ca3891e647f3
김지호 <crsn1111@gmail.com>
2022-09-12 21:20:01 +0900

    [test] 카카오 로그인

--- pages/champions/[champion].tsx
+++ pages/champions/[champion].tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Grid from "../../components/common/Grid";
 import PageContainer from "../../components/common/PageContainer";
 import Seo from "../../components/common/Seo";
-import { useChampion } from "../api/champion";
+import { useChampion } from "../apis/champion";
 
 const Champion = () => {
   const router = useRouter();
