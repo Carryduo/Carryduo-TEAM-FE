7605d7524787c94adb81e46c4db333143436b3b2
김지호 <crsn1111@gmail.com>
2022-09-12 16:42:28 +0900

    [feat] 카카오 로그인 기능 셋팅

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    secret: process.env.SECRET,
+    providers: [
+      KakaoProvider({
+        name: "kakao",
+        clientId: process.env.KAKAO_CLIENT_ID || "",
+        clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+      }),
+    ],
+  });
+}
