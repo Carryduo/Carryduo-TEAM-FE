a2dbafe120ed28fe2b4e1dd80edd01c288aca54e
김지호 <crsn1111@gmail.com>
2022-09-13 13:21:49 +0900

    [style] 스타일 수정

--- components/layouts/Signup/IntroContainer.tsx
+++ components/layouts/Signup/IntroContainer.tsx
@@ -12,10 +12,10 @@ const IntroContainer = () => {
   return (
     <div className="h-full w-[26rem]">
       <Slider {...settings}>
-        <div className="flex h-[500px] w-full items-center justify-center ">
+        <div className="flex h-[550px] w-full items-center justify-center">
           <span>듀오 승률을 확인해보세요</span>
         </div>
-        <div className="flex h-[500px] w-full items-center justify-center ">
+        <div className="flex h-[550px] w-full items-center justify-center">
           <span>듀오를 찾기 위해 정보를 입력해보세요</span>
         </div>
       </Slider>
