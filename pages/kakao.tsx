5898883f26d8bfba04cdb2a783e5fb2ce1298daa
김지호 <crsn1111@gmail.com>
2022-09-15 15:56:29 +0900

    [docs] 문서 수정

--- pages/kakao.tsx
+++ pages/kakao.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-
 // 카카오 로그인 인가코드
 const Kakao = () => {
+  if (typeof window === undefined) return;
   const code = new URL(window.location.href).searchParams.get("code");
-  useEffect(() => {
-    const Kakao = async (code: string) => {
-      // return await instance
-      //   .post(`/auth/kakao?code=${code}`)
-      //   .then(() => {
-      //     // navigate("/projectList");
-      //   })
-      //   .catch(() => {
-      //     // navigate("/")
-      //   });
-    };
-    if (code) {
-      Kakao(code);
-    }
-  }, [code]);
+  console.log(code);
+  // useEffect(() => {
+  //   const Kakao = async (code: string) => {
+  //     return await instance
+  //       .post(`/auth/kakao?code=${code}`)
+  //       .then(() => {
+  //         // navigate("/projectList");
+  //       })
+  //       .catch(() => {
+  //         // navigate("/")
+  //       });
+  //   };
+  //   if (code) {
+  //     Kakao(code);
+  //   }
+  // }, [code]);
 
-  return <div>로그인 페이지</div>;
+  return <div>로그인</div>;
 };
 
 export default Kakao;
