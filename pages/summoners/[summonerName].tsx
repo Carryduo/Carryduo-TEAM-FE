24b51011fb3bead139b22a70912643c4282e4b19
김지호 <crsn1111@gmail.com>
2022-09-23 13:03:10 +0900

    [feat] 기능 수정

--- pages/summoners/[summonerName].tsx
+++ pages/summoners/[summonerName].tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Grid from "../../components/common/Grid";
 import PageContainer from "../../components/common/PageContainer";
 import Seo from "../../components/common/Seo";
@@ -15,18 +16,68 @@ const Summoners = ({ summonerName }: Props) => {
   const { data } = useGetSummoner(summonerName);
   return (
     <PageContainer space="space-x-4">
-      <Seo title={summonerName} />
-      <div className="h-full w-full space-y-4">
-        <Grid width="w-[700px]" height="h-1/2">
-          <span>소환사 정보{summonerName}</span>
+      <Seo title={data === undefined ? "정보없음" : summonerName} />
+      {data === undefined ? (
+        <Grid width="w-full" height="h-full">
+          <span>사용자 정보가 없습니다</span>
         </Grid>
-        <Grid width="w-[700px]" height="h-1/2">
-          <span>소환사 모스트 픽</span>
-        </Grid>
-      </div>
-      <Grid width="w-full" height="h-[calc(100%+1rem)]">
-        <span>평판</span>
-      </Grid>
+      ) : (
+        <>
+          <div className="h-full w-full space-y-4">
+            <Grid width="w-[700px]" height="h-1/2">
+              <div className="flex space-x-4">
+                <span className="text-2xl">{summonerName}</span>
+                <Image
+                  width={50}
+                  height={50}
+                  alt=""
+                  src={
+                    data.data.tierImg === undefined
+                      ? "https://avatars.githubusercontent.com/u/79081800?v=4"
+                      : String(data.data.tierImg)
+                  }
+                />
+                <Image
+                  width={50}
+                  height={50}
+                  alt=""
+                  src={
+                    data.data.summonerIcon === undefined
+                      ? "https://avatars.githubusercontent.com/u/79081800?v=4"
+                      : String(data.data.summonerIcon)
+                  }
+                />
+                <span>{`승률 : ${data.data.winRate}%`}</span>
+              </div>
+            </Grid>
+            <Grid width="w-[700px]" height="h-1/2">
+              <span>소환사 모스트 픽</span>
+              <div className="flex space-x-4">
+                {data.data.mostChamps.map((data,i) => {
+                  return (
+                    <div key={data.id}>
+                      <Image
+                        width={150}
+                        height={150}
+                        alt=""
+                        src={
+                          data.champImg === undefined
+                            ? "https://avatars.githubusercontent.com/u/79081800?v=4"
+                            : String(data.champImg)
+                        }
+                      />
+                      <span>{i+1}등</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </Grid>
+          </div>
+          <Grid width="w-full" height="h-[calc(100%+1rem)]">
+            <span>평판</span>
+          </Grid>
+        </>
+      )}
     </PageContainer>
   );
 };
