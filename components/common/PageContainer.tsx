d6ddcad895aa6eed6612b008471a486ecab2c09b
김지호 <crsn1111@gmail.com>
2022-09-26 19:16:05 +0900

    [style] 페이지 스타일 이벤트 추가

--- components/common/PageContainer.tsx
+++ components/common/PageContainer.tsx
@@ -5,7 +5,7 @@ interface ContainerSizeProp {
 
 const PageContainer = ({ children, space }: ContainerSizeProp) => {
   return (
-    <div className={`relative flex h-[83%] w-full ${space} rounded-xl`}>
+    <div className={`relative flex h-[83%] w-full ${space} rounded-xl animate-wiggle`}>
       {children}
     </div>
   );
