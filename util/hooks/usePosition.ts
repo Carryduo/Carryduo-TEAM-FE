24001a4b7b2b775561470e3ad48dd05d40959119
김지호 <crsn1111@gmail.com>
2022-10-02 00:43:31 +0900

    [feat] custom position image

---
+++ util/hooks/usePosition.ts
@@ -0,0 +1,7 @@
+export const usePosition = (position: number) => {
+  if (position === 1) return "Top";
+  if (position === 2) return "Jungle";
+  if (position === 3) return "Mid";
+  if (position === 4) return "Bot";
+  if (position === 5) return "Support";
+};
