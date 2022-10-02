cd61e8b00ac89550d3a6c535abc88373849117cd
김지호 <crsn1111@gmail.com>
2022-10-02 23:47:20 +0900

    [feat] 전적 도넛 차트

--- components/common/Summoner/HistoryDonut.tsx
+++ components/common/Summoner/HistoryDonut.tsx
@@ -1,20 +1,44 @@
+import { Doughnut } from "react-chartjs-2";
 import { Summoner } from "../../../core/api/summoner";
+import { Chart as ChartJS, ArcElement } from "chart.js";
 
 interface Props {
   Summoner: Summoner;
 }
 
+ChartJS.register(ArcElement);
+
 const HistoryDonut = ({ Summoner }: Props) => {
+  const winRateData = (win: number, lose: number) => {
+    const data = {
+      labels: ["승리", "패배"],
+      datasets: [
+        {
+          data: [win, lose],
+          backgroundColor: ["#5383E8", "#E84057"],
+          radius: 64,
+          borderWidth: 0,
+        },
+      ],
+    };
+    return data;
+  };
   return (
-    <>
-      <span>
+    <div className="flex flex-col items-center">
+      <span className="text-gray-400">
         {`${Summoner.history.total}전 ${Summoner.history.win}승 ${Summoner.history.lose}패`}
       </span>
       <div>
-        <span>그래프</span>
-        <span>{`${Summoner.history.win}승 ${Summoner.history.lose}패 ${Summoner.history.winRate}%`}</span>
+        <div className=" flex h-48 w-48 items-center justify-center">
+          <Doughnut
+            data={winRateData(Summoner.history.win, Summoner.history.lose)}
+            className="h-48 w-48"
+            options={{ cutout: 70 }}
+          ></Doughnut>
+          <span className="absolute z-[1000]">{`${Summoner.history.winRate}%`}</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
