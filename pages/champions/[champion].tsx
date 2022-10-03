9b82a32b3318a666349d8a006abc81aba58cc38d
김지호 <crsn1111@gmail.com>
2022-10-03 15:52:02 +0900

    #77 - [fix] ssr notfound 404 page router

--- pages/champions/[champion].tsx
+++ pages/champions/[champion].tsx
@@ -4,33 +4,26 @@ import PageContainer from "../../components/common/PageContainer";
 import Seo from "../../components/common/Seo";
 import { useRouter } from "next/router";
 import ChampionDetailContainer from "../../components/container/ChampionDetailContainer";
-import { Champions } from "../../core/api/champions";
-import { instance } from "../../core/api/axios";
-import { GetStaticPaths } from "next";
+import { GetServerSideProps } from "next";
 
-interface Prop {
-  champion: string;
-}
-
-interface PageProps {
-  params: Prop;
-}
-
-const Champion = ({ champion }: Prop) => {
+const Champion = () => {
   const { query } = useRouter();
   return (
     <PageContainer space="space-x-4">
       <Seo title={String(query.name)} />
       <div className="h-full w-full space-y-4">
         <Grid width="w-[900px]" height="h-1/2">
-          <ChampionDetailContainer champId={Number(champion)} />
+          <ChampionDetailContainer champId={Number(query.champion)} />
         </Grid>
         <Grid width="w-[900px]" height="h-1/2">
           <span>챔피언 조합 승률 모스트</span>
         </Grid>
       </div>
       <Grid width="w-full min-w-[550px]" height="h-[calc(100%+1rem)]">
-        <CommentsFormContainer category="champ" champId={Number(champion)} />
+        <CommentsFormContainer
+          category="champ"
+          champId={Number(query.champion)}
+        />
       </Grid>
     </PageContainer>
   );
@@ -38,23 +31,30 @@ const Champion = ({ champion }: Prop) => {
 
 export default Champion;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { data: Champions } = await instance.get("/champ");
-  return {
-    paths: Champions.map((data: Champions) => {
-      return {
-        params: {
-          champion: data.id,
-        },
-      };
-    }),
-    fallback: false,
-  };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const { data: Champions } = await instance.get("/champ");
+//   return {
+//     paths: Champions.map((data: Champions) => {
+//       return {
+//         params: {
+//           champion: data.id,
+//         },
+//       };
+//     }),
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps = async (context: PageProps) => {
-  const { champion } = context.params;
-  return {
-    props: { champion },
-  };
+// export const getStaticProps = async (context: PageProps) => {
+//   const { champion } = context.params;
+//   return {
+//     props: { champion },
+//   };
+// };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (context.query.category === undefined) {
+    return { notFound: true };
+  }
+  return { props: {} };
 };
