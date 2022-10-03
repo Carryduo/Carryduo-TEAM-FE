029615f625102f25ea4eaff6c8a788585294a10a
김지호 <crsn1111@gmail.com>
2022-10-03 22:04:35 +0900

    [feat] 듀오 승률 데이터 기능 추가

---
+++ components/container/ChampionWinRateContainer.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { useState } from "react";
+import { useGetDuoChampRank } from "../../core/api/duoChampDetail";
+
+interface Props {
+  category: string;
+}
+
+const ChampionWinRateContainer = ({ category }: Props) => {
+  const [pick, setPick] = useState<string>("top");
+  const { data: Duo } = useGetDuoChampRank(category, pick);
+  return (
+    <div className=" h-full w-full p-2">
+      <span>듀오 승률 </span>
+      <header className=" mb-2 flex h-7 w-full justify-around">
+        <span onClick={() => setPick("top")}>탑</span>
+        <span onClick={() => setPick("jungle")}>정글</span>
+        <span onClick={() => setPick("mid")}>미드</span>
+        <span onClick={() => setPick("ad")}>원딜</span>
+        <span onClick={() => setPick("support")}>서폿</span>
+      </header>
+      <div className="flex space-x-7">
+        {Duo?.length === undefined ? (
+          <span>데이터 없음</span>
+        ) : (
+          Duo.map((data) => {
+            return (
+              <div key={data.id}>
+                <Image
+                  alt=""
+                  src={data.subChampId.champImg}
+                  width={150}
+                  height={150}
+                  layout="fixed"
+                  placeholder="blur"
+                  blurDataURL={data.subChampId.champImg}
+                />
+                <span>{`${data.winrate}%`}</span>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChampionWinRateContainer;
--- components/container/DuoRankContainer.tsx
+++ components/container/DuoRankContainer.tsx
@@ -1,39 +1,55 @@
+import Image from "next/image";
 import { Fragment, useState } from "react";
-import { useQuery } from "react-query";
-import { getDuo } from "../../core/api/duoChamps";
+import { useGetDuoRank } from "../../core/api/duoChamps";
 
 const DuoRankContainer = () => {
-  const [pick, setPick] = useState<string>("combination");
-  const { data } = useQuery(["getDuo", pick], () => getDuo(pick));
+  const [pick, setPick] = useState<string>("top-jungle");
+  const { data } = useGetDuoRank(pick);
   return (
     <Fragment>
       <header className=" mb-2 flex h-7 w-full justify-around">
-        <span onClick={() => setPick("combination")}>탑 + 정글</span>
-        <span onClick={() => setPick("combination1")}>미드 + 정글</span>
-        <span onClick={() => setPick("combination2")}>원딜 + 서폿</span>
+        <span onClick={() => setPick("top-jungle")}>탑 + 정글</span>
+        <span onClick={() => setPick("mid-jungle")}>미드 + 정글</span>
+        <span onClick={() => setPick("ad-support")}>원딜 + 서폿</span>
       </header>
       <div className="grid w-full grid-cols-5">
-        <span className="text-center">순위</span>
+        <span>순위</span>
         <span>챔피언</span>
         <span>티어</span>
-        <span>승리횟수</span>
+        <span>승률</span>
         <span>매칭 수</span>
       </div>
       <div className="h-[calc(100%-4rem)] overflow-y-scroll">
         <div className="w-full">
-          {data?.map((data) => {
+          {data?.map((data, i) => {
             return (
               <div
                 className=" grid grid-cols-5 grid-rows-[repeat(auto-fill,50px)]"
                 key={data.id}
               >
-                <span>{data.id}</span>
-                <div>
-                  <span>{data.champ1}</span>
-                  <span>{data.champ2}</span>
+                <span>{i + 1}</span>
+                <div className="flex space-x-2">
+                  <Image
+                    alt=""
+                    src={data.mainChampId.champImg}
+                    width={56}
+                    height={56}
+                    layout="fixed"
+                    placeholder="blur"
+                    blurDataURL={data.mainChampId.champImg}
+                  />
+                  <Image
+                    alt=""
+                    src={data.subChampId.champImg}
+                    width={56}
+                    height={56}
+                    layout="fixed"
+                    placeholder="blur"
+                    blurDataURL={data.subChampId.champImg}
+                  />
                 </div>
                 <span>{data.tier}</span>
-                <span>{data.winRate}</span>
+                <span>{`${data.winrate}%`}</span>
                 <span>{data.sampleNum}</span>
               </div>
             );
