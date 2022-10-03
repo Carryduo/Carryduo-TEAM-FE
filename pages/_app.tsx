0fb1a1e01aa4a7f7ce4b195a3a120de4852c75ce
김지호 <crsn1111@gmail.com>
2022-10-03 15:49:58 +0900

    [feat] custom window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from "next/app";
 import HeaderMain from "../components/container/Main/HeaderContainer";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MainContainer from "../components/container/Main/MainContainer";
 import { RecoilRoot } from "recoil";
-import Router from "next/router";
 import LoadingContainer from "../components/layouts/Handler/LoadingContainer";
 import "../styles/globals.css";
-import "../styles/slick.css"
+import "../styles/slick.css";
+import { useWindow } from "../util/hooks/useWindow";
+import PageContainer from "../components/common/PageContainer";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,48 +24,28 @@ export const queryClient = new QueryClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isWindows, setIsWindows] = useState<boolean>(true);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const start = () => {
-      setLoading(true);
-    };
-    const end = () => {
-      setLoading(false);
-    };
-    const window =
-      navigator.userAgent.includes("Windows") ||
-      navigator.userAgent.includes("Macintosh");
-    setIsWindows(Boolean(window));
-
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
-
-    return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
-  }, [pageProps]);
+  const window = useWindow();
   return (
     <>
-      {isWindows ? (
-        <RecoilRoot>
-          <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-              <ReactQueryDevtools initialIsOpen={true} />
-              <MainContainer>
-                <HeaderMain />
-                {loading ? <LoadingContainer /> : <Component {...pageProps} />}
-              </MainContainer>
-            </Hydrate>
-          </QueryClientProvider>
-        </RecoilRoot>
-      ) : (
-        "mobile 환경은 지원하지 않습니다 pc로 접속해주세요"
-      )}
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <ReactQueryDevtools initialIsOpen={true} />
+            <MainContainer>
+              {window ? (
+                <>
+                  <HeaderMain />
+                  <Component {...pageProps} />
+                </>
+              ) : (
+                <PageContainer>
+                  <LoadingContainer text="Mobile 환경에서는 해당 사이트를 이용하실 수 없습니다" />
+                </PageContainer>
+              )}
+            </MainContainer>
+          </Hydrate>
+        </QueryClientProvider>
+      </RecoilRoot>
     </>
   );
 }
