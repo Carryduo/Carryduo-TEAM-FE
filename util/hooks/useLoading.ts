91435ac366bb6e79e08b0aeeed72dc7fe4e9cb81
김지호 <crsn1111@gmail.com>
2022-10-03 15:50:07 +0900

    [feat] custom loading

---
+++ util/hooks/useLoading.ts
@@ -0,0 +1,26 @@
+import { Router } from "next/router";
+import { useEffect, useState } from "react";
+
+export const useLoading = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return loading;
+};
