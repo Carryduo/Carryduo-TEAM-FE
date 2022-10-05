58561793c5524b0b36f7fe9f4713e4e9ca024d60
김지호 <crsn1111@gmail.com>
2022-10-05 21:57:41 +0900

    [feat] 기능 수정

--- components/container/ChampionWinRateContainer.tsx
+++ components/container/ChampionWinRateContainer.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { useGetDuoChampRank } from "../../core/api/duoChampion/queries";
+import DuoRankPickLine from "../common/DuoRank/DuoRankPickLine";
+import Position from "../../core/config/ChampionPosition.json";
 
 interface Props {
   category: string;
@@ -12,21 +14,27 @@ const ChampionWinRateContainer = ({ category }: Props) => {
   const { data: Duo } = useGetDuoChampRank(category, pick);
   return (
     <div className=" h-full w-full p-2">
-      <span>듀오 승률 </span>
       <header className=" mb-2 flex h-7 w-full justify-around">
-        <span onClick={() => setPick("top")}>탑</span>
-        <span onClick={() => setPick("jungle")}>정글</span>
-        <span onClick={() => setPick("mid")}>미드</span>
-        <span onClick={() => setPick("ad")}>원딜</span>
-        <span onClick={() => setPick("support")}>서폿</span>
+        <span className="text-gray-300">라인별 듀오 챔피언</span>
+        {Position.ChampionWinRatedata.map((data) => {
+          return (
+            <div
+              key={data.id}
+              className="flex w-[50px] cursor-pointer flex-col text-center"
+            >
+              <span onClick={() => setPick(data.id)}>{data.position}</span>
+              {pick === data.id && <DuoRankPickLine animate="animate-smLine" />}
+            </div>
+          );
+        })}
       </header>
-      <div className="flex space-x-7">
+      <div className="flex space-x-12 justify-center items-center h-full w-full pb-12">
         {Duo?.length === undefined ? (
           <span>데이터 없음</span>
         ) : (
           Duo.map((data) => {
             return (
-              <div key={data.id} className="flex flex-col">
+              <div key={data.id} className="flex flex-col items-center space-y-4">
                 <Link
                   href={{
                     pathname: `/champions/${data.subChampId.id}`,
@@ -40,15 +48,14 @@ const ChampionWinRateContainer = ({ category }: Props) => {
                     <Image
                       alt=""
                       src={data.subChampId.champImg}
-                      width={150}
-                      height={150}
+                      width={100}
+                      height={100}
                       layout="fixed"
-                      className="cursor-pointer"
                       loading="eager"
                     />
                   </a>
                 </Link>
-                <span>{`${data.winrate}%`}</span>
+                <span>{`승률 - ${data.winrate}%`}</span>
               </div>
             );
           })
