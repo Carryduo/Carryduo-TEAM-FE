72322b42b9d755f62fdfc6a393995dfa4d4bfc85
김지호 <crsn1111@gmail.com>
2022-10-06 19:06:47 +0900

    [style] input style 수정

--- components/layouts/Champion/MostSummonerData.tsx
+++ components/layouts/Champion/MostSummonerData.tsx
@@ -6,20 +6,22 @@ interface Props {
 }
 const MostSummonerData = ({ MostSummoner }: Props) => {
   return (
-    <>
+    <div className="flex h-[200px] flex-col  space-y-2 overflow-y-scroll">
       <span>선호 챔피언 유저</span>
       {MostSummoner.length === 0 ? (
         <span>없음</span>
       ) : (
         MostSummoner.map((data) => {
           return (
-            <span key={data.id}>
-              {data.nickname} / {useTier(data.tier)}
-            </span>
+            <>
+              <span key={data.id}>
+                {data.nickname} / {useTier(data.tier)}
+              </span>
+            </>
           );
         })
       )}
-    </>
+    </div>
   );
 };
 
