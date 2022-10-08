48427e6b3d4e42e7d0c79e947c1ac777787d61f8
김지호 <crsn1111@gmail.com>
2022-10-08 20:58:15 +0900

    [feat] 가상 대전 기능 추가

---
+++ components/layouts/Simulation/PickLayout.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+import { ChampionProps } from "../../../core/config/pickChampion";
+import ChampionsContainer from "../../container/ChampionsContainer";
+
+interface Props {
+  pick: ChampionProps[];
+  SelectChampion: () => void;
+}
+
+const PickLayout = ({ pick, SelectChampion }: Props) => {
+  const {
+    query: { position },
+  } = useRouter();
+  return (
+    <div className="flex h-full w-[40%] flex-col justify-between">
+      <ChampionsContainer
+        gridCol="grid-cols-7"
+        width="w-full"
+        height="h-[calc(100%-10rem)]"
+        toLink={false}
+      />
+      <div className="flex h-24 w-full items-center justify-center">
+        {pick[3]?.name === undefined ? (
+          <div
+            onClick={SelectChampion}
+            className="flex h-10 w-28 cursor-pointer items-center justify-center bg-blue-400"
+          >
+            <span>선택하기</span>
+          </div>
+        ) : (
+          <div
+            onClick={() => {
+              console.log(position, pick);
+            }}
+            className="flex h-10 w-28 cursor-pointer items-center justify-center bg-blue-400"
+          >
+            <span>시작하기</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PickLayout;
