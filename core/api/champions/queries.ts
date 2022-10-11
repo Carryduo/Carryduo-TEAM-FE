8701060bff8354cd9afc57741d28bdd6e9b3d096
김지호 <crsn1111@gmail.com>
2022-10-12 00:45:13 +0900

    #115 - [fix] 이름만 가져오는 데이터로 재가공

--- core/api/champions/queries.ts
+++ core/api/champions/queries.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { ErrorHandle } from "../../config/ErrorType";
-import { Champions, IChampions } from "./types";
+import { instance } from "../axios";
+import { Champions, IChampionName, IChampions } from "./types";
 
-export const useGetChamps = (locale?: string) => {
+export const useGetChamps = () => {
   return useQuery<IChampions, ErrorHandle, Champions[]>(
     ["Champs"],
+    () => {
+      return instance.get("/champ");
+    },
+    {
+      select: (data) => data.data,
+    }
+  );
+};
+
+export const useGetChampionName = (locale?: string) => {
+  return useQuery<IChampionName, ErrorHandle, string[]>(
+    ["ChampName"],
     () => {
       return axios.get(`/api/locale?locale=${locale}`);
     },
