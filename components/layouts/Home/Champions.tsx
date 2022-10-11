98dcd1051e968f6477af0129bc1560ffea1e7b70
김지호 <crsn1111@gmail.com>
2022-10-11 18:54:12 +0900

    [feat] 다국어 검색 기능

--- components/layouts/Home/Champions.tsx
+++ components/layouts/Home/Champions.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import React from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { useGetChamps } from "../../../core/api/champions/queries";
+import { getLocale } from "../../../core/config/locale";
 import { PickChampArray } from "../../../core/config/PickChampArray";
 import { PickChampion } from "../../../core/config/pickChampion";
 import { PickCnt } from "../../../core/config/PickCnt";
+import { getShadowColor } from "../../../core/config/shadowColor";
+import { useShadow } from "../../../util/hooks/useShadow";
 
 interface ChampionsContainerProps {
   value: string;
@@ -13,15 +16,25 @@ interface ChampionsContainerProps {
 }
 
 const ChampionsList = ({ value, toLink }: ChampionsContainerProps) => {
-  const { data: Champions } = useGetChamps();
+  const locale = useRecoilValue(getLocale);
+  const { data: Champions } = useGetChamps(locale);
   const setChampion = useSetRecoilState(PickChampion);
   const [cnt, setCnt] = useRecoilState(PickCnt);
   const pickChamp = useRecoilValue(PickChampArray).map((data) => data.id);
+  const [color] = useShadow(getShadowColor);
   const ChampionList = Champions?.filter((val) => {
-    if (value === "") {
-      return val;
-    } else if (val.champNameKo.includes(value)) {
-      return val;
+    if (locale === "ko") {
+      if (value === "") {
+        return val;
+      } else if (val.champNameKo.includes(value)) {
+        return val;
+      }
+    } else {
+      if (value === "") {
+        return val;
+      } else if (val.champNameEn.includes(value)) {
+        return val;
+      }
     }
   }).filter((val) => {
     return !pickChamp.includes(val.id);
@@ -37,26 +50,39 @@ const ChampionsList = ({ value, toLink }: ChampionsContainerProps) => {
           return (
             <div key={i} className="h-[85px] w-14 cursor-pointer text-center">
               {toLink ? (
-                <Link
-                  as={`/champions/${data.id}`}
-                  href={{
-                    pathname: `/champions/${data.id}`,
-                    query: { name: data.champNameKo, category: "champ" },
-                  }}
-                  passHref
-                >
-                  <a>
-                    <Image
-                      alt={`${data.champNameEn}`}
-                      src={`https://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${data.champNameEn}.png`}
-                      width={56}
-                      height={56}
-                      priority
-                      placeholder="blur"
-                      blurDataURL={`https://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${data.champNameEn}.png`}
-                    />
-                  </a>
-                </Link>
+                <div>
+                  <Link
+                    as={`/champions/${data.id}`}
+                    href={{
+                      pathname: `/champions/${data.id}`,
+                      query: { name: data.champNameKo, category: "champ" },
+                    }}
+                    passHref
+                  >
+                    <a>
+                      <Image
+                        alt={`${data.champNameEn}`}
+                        src={`https://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${data.champNameEn}.png`}
+                        width={56}
+                        height={56}
+                        priority
+                        placeholder="blur"
+                        blurDataURL={`https://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${data.champNameEn}.png`}
+                        className={`hover:border-[1px] hover:border-solid ${
+                          color === 1
+                            ? "hover:border-blue-100"
+                            : color === 2
+                            ? "hover:border-[#5F99FF]"
+                            : color === 3
+                            ? "hover:border-[#00D39E]"
+                            : color === 4
+                            ? "hover:border-[#FF7637]"
+                            : "hover:border-gray-700"
+                        }`}
+                      />
+                    </a>
+                  </Link>
+                </div>
               ) : (
                 <Image
                   alt={`${data.champNameEn}`}
@@ -75,12 +101,23 @@ const ChampionsList = ({ value, toLink }: ChampionsContainerProps) => {
                     });
                     setCnt(cnt + 1);
                   }}
+                  className={`hover:border-[1px] hover:border-solid ${
+                    color === 1
+                      ? "hover:border-blue-100 active:border-[2px]"
+                      : color === 2
+                      ? "hover:border-[#5F99FF] active:border-[2px]"
+                      : color === 3
+                      ? "hover:border-[#00D39E] active:border-[2px]"
+                      : color === 4
+                      ? "hover:border-[#FF7637] active:border-[2px]"
+                      : "hover:border-gray-700"
+                  }`}
                 />
               )}
               <span className="whitespace-nowrap text-xs font-medium">
                 {data.champNameKo.length <= 5
-                  ? data.champNameKo
-                  : `${data.champNameKo.substring(0, 3)}...`}
+                  ? data.champ
+                  : `${String(data.champ).substring(0, 3)}...`}
               </span>
             </div>
           );
