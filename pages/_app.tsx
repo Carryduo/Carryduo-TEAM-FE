9c1ac336df4d82f6738b303711140168c54339da
김지호 <crsn1111@gmail.com>
2022-10-19 19:13:26 +0900

    #76 - [refactor] queryClient 재설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,39 @@
 import type { AppProps } from "next/app";
 import HeaderMain from "../components/container/Main/HeaderContainer";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import React from "react";
+import React, { useState } from "react";
 import { RecoilRoot } from "recoil";
 import LoadingContainer from "../components/layouts/Handler/LoadingContainer";
 import "../styles/globals.css";
 import "../styles/slick.css";
 import { useWindow } from "../util/hooks/useWindow";
 import PageContainer from "../components/common/PageContainer";
-import dynamic from "next/dynamic";
-const MainContainer = dynamic(
-  () => import("../components/container/Main/MainContainer"),
-  { ssr: false }
-);
+import MainContainer from "../components/container/Main/MainContainer";
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: false,
-      cacheTime: 1000 * 60 * 60 * 24 * 7,
-    },
-  },
-});
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
   const window = useWindow();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            retry: false,
+            cacheTime: 1000 * 60 * 60 * 24 * 7,
+          },
+        },
+      })
+  );
   return (
     <>
       <RecoilRoot>
