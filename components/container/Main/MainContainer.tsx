4c6678a9957f2b3dd9168406a27c1309ad7484ca
김지호 <crsn1111@gmail.com>
2022-10-21 00:06:49 +0900

    [feat] 이미지 변경

--- components/container/Main/MainContainer.tsx
+++ components/container/Main/MainContainer.tsx
@@ -12,16 +12,23 @@ const MainContainer = ({ children }: DarkContainerProps) => {
   const [color] = useShadow(getShadowColor);
   return (
     <div className="relative flex h-screen min-h-[800px] w-full min-w-[1640px] items-center justify-center bg-black bg-opacity-50">
-      <Image
+      <img
         alt="Summonersrift"
-        layout="fill"
-        quality={10}
-        src={mainImage}
-        className="absolute -z-50"
-        priority
+        src="/summonersrift.jpg"
+        className="absolute -z-50 h-screen w-full"
       />
       <div
-        className={`z-50 h-[85%] w-[80%] overflow-hidden rounded-xl bg-black bg-opacity-30 px-10 shadow-[0_0_40px_1px] shadow-[#5F99FF]`}
+        className={`z-50 h-[85%] w-[80%] overflow-hidden rounded-xl bg-black bg-opacity-30 px-10 shadow-[0_0_40px_1px] ${
+          color === 1
+            ? "shadow-blue-100"
+            : color === 2
+            ? "shadow-[#5F99FF]"
+            : color === 3
+            ? "shadow-[#00D39E]"
+            : color === 4
+            ? "shadow-[#FF7637]"
+            : "shadow-gray-700"
+        }`}
       >
         {children}
       </div>
