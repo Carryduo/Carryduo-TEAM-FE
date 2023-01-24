ac5cb19894830a728d87aa906aba7f9103366705
김지호 <crsn1111@gmail.com>
2023-01-24 22:18:07 +0900

    [fix] charSet 수정

--- components/layouts/Signup/IntroContainer.tsx
+++ components/layouts/Signup/IntroContainer.tsx
@@ -25,7 +25,6 @@ const IntroContainer = () => {
       <link
         rel="stylesheet"
         type="text/css"
-        charSet="UTF-8"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
